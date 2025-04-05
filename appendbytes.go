cc41975d855f2ecf72ea43613b842e466a5e5fd3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-04-05 05:32:03 -0700

    raft.AppendBytes()

--- appendbytes.go
+++ appendbytes.go
@@ -3,12 +3,12 @@ package raft
 // AppendBytes embeds a file in a raft-file with the provided file-name and the contents of the file provided as []byte.
 //
 // Use this if you need the write to be atomic.
-func AppendBytes(writer Writer, filename string, bytes []byte) error {
+func AppendBytes(writer Writer, filename string, content []byte) error {
 
 	var buffer [256]byte
 	var p []byte = buffer[0:0]
 
-	p = gatherBytes(p, filename, bytes)
+	p = gatherBytes(p, filename, content)
 
 	return putBytes(writer, p)
 }
